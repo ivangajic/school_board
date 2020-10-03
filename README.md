8985a9eec76b7c74ddf52d9fd6dbd37251389b8c
ivangajic <ivangajic@hotmail.com>
2020-10-03 13:30:17 +0200

    Basic setup and database connection

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# school_board
+# Foobar
+
+School Board is web application for keeping track of students and grades
+
+## Installation
+
+Clone repository in local web server folder, or run it by 
+
+```bash
+php -Slocalhost:8080
+```
+or whatever port you want.
+
+Import Database.sql file in your mysql server.
+
+Change config file for your MySQL config (server, username, password, dbname if you change it)
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('lib/Database.php');
+
+$db = new Database();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School Board</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-2">
+            <div class="col-9">
+                <h1>School board</h1>
+            </div>
+            <div class="col-3">
+                <button class="btn btn-success">Add student</button>
+            </div>
+        </div>
+        <hr>
+
+        <div class="row">
+            
+        </div>
+    </div>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+</body>
+</html>
